b4f307e78567e1c8bab583a359addedc27611b73
Sydney Kahn <skahn2022@erhsnyc.net>
2021-07-29 09:53:24 -0400

    fix the button

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 (function(){
 	function buildQuiz(){
-	  // variable to store the HTML output
+
 	  const output = [];
   
-	  // for each question...
 	  myQuestions.forEach(
 		(currentQuestion, questionNumber) => {
   
-		  // variable to store the list of possible answers
+
 		  const answers = [];
   
-		  // and for each available answer...
 		  for(letter in currentQuestion.answers){
   
-			// ...add an HTML radio button
 			answers.push(
 			  `<label>
 				<input type="radio" name="question${questionNumber}" value="${letter}">
@@ -23,7 +20,6 @@
 			);
 		  }
   
-		  // add this question and its answers to the output
 		  output.push(
 			`<div class="question"> ${currentQuestion.question} </div>
 			<div class="answers"> ${answers.join('')} </div>`
@@ -31,42 +27,33 @@
 		}
 	  );
   
-	  // finally combine our output list into one string of HTML and put it on the page
 	  quizContainer.innerHTML = output.join('');
 	}
   
 	function showResults(){
   
-	  // gather answer containers from our quiz
 	  const answerContainers = quizContainer.querySelectorAll('.answers');
   
-	  // keep track of user's answers
 	  let numCorrect = 0;
   
-	  // for each question...
 	  myQuestions.forEach( (currentQuestion, questionNumber) => {
   
-		// find selected answer
+
 		const answerContainer = answerContainers[questionNumber];
 		const selector = `input[name=question${questionNumber}]:checked`;
 		const userAnswer = (answerContainer.querySelector(selector) || {}).value;
   
-		// if answer is correct
+
 		if(userAnswer === currentQuestion.correctAnswer){
-		  // add to the number of correct answers
 		  numCorrect++;
   
-		  // color the answers green
 		  answerContainers[questionNumber].style.color = 'lightgreen';
 		}
-		// if answer is wrong or blank
 		else{
-		  // color the answers red
 		  answerContainers[questionNumber].style.color = 'red';
 		}
 	  });
   
-	  // show number of correct answers out of total
 	  resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
 	}
   
@@ -130,9 +117,7 @@
 	//   }
 	];
   
-	// Kick things off
 	buildQuiz();
   
-	// Event listeners
 	submitButton.addEventListener('click', showResults);
   })();
